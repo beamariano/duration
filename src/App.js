8abe68685289db4bede77716ec95bf2dd2730330
beam <64287361+beamariano@users.noreply.github.com>
2022-01-01 17:20:50 +0800

    fixed bug. able to render in and out

--- src/App.js
+++ src/App.js
@@ -1,93 +1,105 @@
-import './App.css';
+import "./App.css";
 
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 function App() {
-
   const date = new Date();
+  const options = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    weekday: "short",
+    hour12: true,
+  };
 
-  const options = { year: 'numeric', month: 'short', day: 'numeric', weekday: 'short', hour12: true };
-  
   const [now, setNow] = useState({
     dateToday: date.toLocaleDateString(undefined, options),
     timeNow: date.toLocaleTimeString(),
   });
 
   const [log, setLog] = useState([
-    { date: "" }
-  ]
-  );
+    // { date: null, in: null, out: null }
+  ]);
 
-  // console.log(log.date)
+  // updates time every second
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const date = new Date();
+      setNow({
+        dateToday: date.toLocaleDateString(),
+        timeNow: date.toLocaleTimeString(),
+      });
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [now]);
 
-  // // updates time every second
-  // useEffect(() => {
-  //   const timer = setInterval(() => {
-  //     const date = new Date();
-  //     setNow({
-  //       dateToday: date.toLocaleDateString(undefined, options),
-  //       timeNow: date.toLocaleTimeString()
-
-  //     });
-  //   }, 1000);
-  //   return () => clearInterval(timer);
-  // }, [now]);
-
-// logs in time check IN when clicked
+  // logs IN time when clicked
   const checkInHandler = () => {
-    let entry = new Date();
-    setLog(entry);
-    console.log(log)
-  
-    console.log("IN - ", new Date());
-  }
+    let currentDate = new Date();
+    let temp = log;
+    temp.push({
+      date: currentDate.toLocaleDateString(),
+      in: currentDate.toLocaleTimeString(),
+    });
+    setLog(temp);
+  };
 
-  // logs in time check OUT when clicked
-  const checkOutHandler = () => {  console.log("OUT - ",new Date());
-  }
+  // logs OUT time when clicked
+  const checkOutHandler = () => {
+    let currentDate = new Date();
+    let temp = log;
+    temp.push({
+      date: currentDate.toLocaleDateString(),
+      out: currentDate.toLocaleTimeString(),
+    });
+    setLog(temp);
+  };
 
   return (
     <div className="App">
       <header>
-<h1>Time Keeper App</h1>
+        <h1>Time Log App</h1>
       </header>
-      <main>      
+      <main>
         <section className="summary">
-          <h2>Date</h2>         
-         {now.dateToday}
-          <h2>Time</h2>
-         {now.timeNow}         
-        </section> 
-         <section className='controls'>
-        <button onClick={()=> checkInHandler()}>Check In</button>
-          <button onClick={()=> checkOutHandler()}>Check Out</button>
+          <p>
+            <strong>Today's Date</strong> {now.dateToday}
+            <br />
+            <strong>Time Now</strong> {now.timeNow}
+          </p>
+        </section>
+        <section className="controls">
+          <button onClick={() => checkInHandler()}>Check In</button>
+          <button onClick={() => checkOutHandler()}>Check Out</button>
         </section>
         <section className="log">
           <table>
             <thead>
               <tr>
-             
+                <td>#</td>
                 <td>Date</td>
                 <td>In</td>
                 <td>Out</td>
-                <td>Duration</td>
+                {/* //TODO: <td>Duration</td> */}
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>
-                  Hello!</td>
-             
-             
-              {log.map((item) => {
-                <td>{item.log}</td>
+              {log.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td>
+                      {index + 1} {")  "}
+                    </td>
+                    <td>{item.date}</td>
+                    <td> {item.in}</td>
+                    <td> {item.out}</td>
+                  </tr>
+                );
               })}
- </tr>
             </tbody>
-                      </table>
+          </table>
         </section>
-        
-        </main>
+      </main>
     </div>
   );
 }
